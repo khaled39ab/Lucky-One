aa6f480ab39193d5a29b0c84a2ebaec986c7caaa
khaled39ab <khaled39ab@gmail.com>
2022-05-22 21:45:07 +0600

    question answer

--- src/components/QuestionAnswer/QuestionAnswer.js
+++ src/components/QuestionAnswer/QuestionAnswer.js
@@ -12,7 +12,11 @@ const QuestionAnswer = () => {
             <p>* Props can change in child component but state are not.</p>
 
             <h3>How useState works?</h3>
-            <p></p>
+            <p>useState() is a built-in react hook. It's enables the use of state of functional component.</p>
+            <p>When pass the initial state to this function , it returns a variable with the current state value and another function to update this value.</p>
+            <p>useState should only be used within functioning components.</p>
+            <p>If you want to change state using the previous value, you'll need to pass a function that takes the previous value and returns an updated value, such as setMessage(previousVal =&gt; previousVal + currentVal).</p>
+            <p>Using the useState hook inside a function component, we may construct a piece of state without having to switch to class components.</p>
         </div>
     );
 };
